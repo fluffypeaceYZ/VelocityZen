a7470a1e750ca2208922a8791f86fbf265cf64f9
Unknown <buzgata@yahoo.ca>
2018-01-26 05:40:27 -0500

    UFO Strafe

--- Assets/Scripts/UFOScript.cs
+++ Assets/Scripts/UFOScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UFOScript : MonoBehaviour {
 
+	public float UFOSpeed;
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,8 @@ public class UFOScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		transform.position = new Vector3(Mathf.PingPong (Time.time * UFOSpeed, 16) -8, transform.position.y ,transform.position.z);
 
 		transform.position += new Vector3(0, 0, 0.4f);
 		
