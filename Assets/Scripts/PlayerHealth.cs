dffbb38b9e666a73d0127a5b2a044a0c886c8737
Unknown <buzgata@yahoo.ca>
2018-01-26 20:34:54 -0500

    Health Bar Implementation

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public GameObject GameOverScreen;
+
+	public Sprite[] healthBarArray;
+	public Image healthBar;
+
+	private int healthBarCount = 0;
+
+	void Start () 
+	{
+
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			TakeDamage ();
+
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			SceneManager.LoadScene ("Sneak_FPS");
+
+		}
+
+
+	}
+
+	public void TakeDamage()
+	{
+		healthBarCount++;
+
+		if (healthBarCount > 11){
+
+			GameOver ();
+		}
+
+		healthBar.sprite = healthBarArray [healthBarCount];
+	}
+
+	public void HealDamage(){
+
+		healthBarCount = 0;
+		healthBar.sprite = healthBarArray [healthBarCount];
+	}
+
+	void OnCollisionEnter (Collider other) {
+
+		if(other.gameObject.tag == "Laser")
+		{
+			TakeDamage ();
+		}
+
+		if(other.gameObject.tag == "Health")
+		{
+			HealDamage();
+			Destroy (other.gameObject, 1);
+
+
+		}
+
+	}
+
+	void GameOver()
+	{
+		GameOverScreen.SetActive (true);
+	}
+}
