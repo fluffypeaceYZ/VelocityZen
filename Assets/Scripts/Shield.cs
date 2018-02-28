c82fe7f06c7a410c77b03cadb6ba56b151c383ad
Unknown <buzgata@yahoo.ca>
2018-02-28 00:46:22 -0500

    Added Transparent Shield

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Shield : MonoBehaviour {
 	float BottomScreen = 10f;
 	float UpperScreen = -7.4f;
-	public GameObject bullet;
-	public GameObject shield;
+
 	 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +17,7 @@ public class Shield : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Physics.IgnoreCollision(bullet.GetComponent<Collider>(), shield.GetComponent<Collider>());
+		 
 
 		transform.position += new Vector3(0, 0, 0.4f);
 
