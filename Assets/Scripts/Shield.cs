740c8a30e6cdbc2c65a33a6566f857a39b3b9593
Unknown <buzgata@yahoo.ca>
2018-02-27 22:48:34 -0500

    Random

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -6,17 +6,19 @@ public class Shield : MonoBehaviour {
 	float BottomScreen = 10f;
 	float UpperScreen = -7.4f;
 	public GameObject bullet;
+	public GameObject shield;
+	 
 	// Use this for initialization
 	void Start () {
 
+		 
 
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+		Physics.IgnoreCollision(bullet.GetComponent<Collider>(), shield.GetComponent<Collider>());
 
 		transform.position += new Vector3(0, 0, 0.4f);
 
