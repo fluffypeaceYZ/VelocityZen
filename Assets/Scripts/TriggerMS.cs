f1d0e792340fe4f840f4a4e99b80cb6c03ff8ab0
Unknown <buzgata@yahoo.ca>
2018-03-05 09:44:02 -0500

    Fixed it!

--- Assets/Scripts/TriggerMS.cs
+++ Assets/Scripts/TriggerMS.cs
@@ -8,14 +8,10 @@ public class TriggerMS : MonoBehaviour {
 	public GameObject ShieldCharger;
 	public GameObject TriggerZoneMS;
 	public GameObject Player;
-	GameObject MedKitInstance;
-	GameObject ShieldChargerInstance;
-	GameObject TriggerZoneMSInstance;
-	bool canCreateTrigger;
 
 	// Use this for initialization
 	void Start () {
-		canCreateTrigger = false;
+
 		
 	}
 	
@@ -26,23 +22,18 @@ public class TriggerMS : MonoBehaviour {
 		
 	}
 
-	void CreateTrigger(){
-	
-		if (canCreateTrigger = true) {
 
-			TriggerZoneMSInstance = Instantiate (TriggerZoneMS, new Vector3 (9, -1.5f, Player.transform.position.z + 200), TriggerZoneMS.transform.rotation) as GameObject;
-			canCreateTrigger = false;
-			print ("Created TriggerMS");
 
-		}
-	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
 			print ("TRIGGERMS COLLISION");
-			MedKitInstance =  Instantiate (MedKit, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z + Random.Range (100,150)), MedKit.transform.rotation) as GameObject;
-			ShieldChargerInstance = Instantiate (ShieldCharger, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z +  Random.Range (100,150)), ShieldCharger.transform.rotation) as GameObject;
-
+			GameObject MedKitInstance;
+			MedKitInstance =  Instantiate (MedKit, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z + Random.Range (100,300)), MedKit.transform.rotation) as GameObject;
+			GameObject ShieldChargerInstance;
+			ShieldChargerInstance = Instantiate (ShieldCharger, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z +  Random.Range (100,300)), ShieldCharger.transform.rotation) as GameObject;
+			GameObject TriggerZoneMSInstance;
+			TriggerZoneMSInstance = Instantiate (TriggerZoneMS, new Vector3 (9, -1.5f, Player.transform.position.z + 1000), TriggerZoneMS.transform.rotation) as GameObject;
 		}
 
 
