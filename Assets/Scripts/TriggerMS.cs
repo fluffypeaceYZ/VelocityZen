93d6958e0aac5214377c4b53cf73c91052be1289
Unknown <buzgata@yahoo.ca>
2018-03-05 08:13:13 -0500

    TriggerZoneMS Bool doesn't work

--- Assets/Scripts/TriggerMS.cs
+++ Assets/Scripts/TriggerMS.cs
@@ -11,27 +11,38 @@ public class TriggerMS : MonoBehaviour {
 	GameObject MedKitInstance;
 	GameObject ShieldChargerInstance;
 	GameObject TriggerZoneMSInstance;
-	private bool canCreateTrigger = false;
+	bool canCreateTrigger;
 
 	// Use this for initialization
 	void Start () {
+		canCreateTrigger = false;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 		
+	}
+
+	void CreateTrigger(){
+	
+		if (canCreateTrigger = true) {
+
+			TriggerZoneMSInstance = Instantiate (TriggerZoneMS, new Vector3 (9, -1.5f, Player.transform.position.z + 200), TriggerZoneMS.transform.rotation) as GameObject;
+			canCreateTrigger = false;
+			print ("Created TriggerMS");
+
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
-			canCreateTrigger = true;
 			print ("TRIGGERMS COLLISION");
 			MedKitInstance =  Instantiate (MedKit, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z + Random.Range (100,300)), MedKit.transform.rotation) as GameObject;
 			ShieldChargerInstance = Instantiate (ShieldCharger, new Vector3 (Random.Range (-7,7), 0, Player.transform.position.z +  Random.Range (100,300)), ShieldCharger.transform.rotation) as GameObject;
-			TriggerZoneMSInstance = Instantiate (TriggerZoneMS, new Vector3 (9, -1.5f, Player.transform.position.z + 500), TriggerZoneMS.transform.rotation) as GameObject;
-			canCreateTrigger = false;
-		
+			canCreateTrigger = true; 
 		}
 
 
