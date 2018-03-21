7361cdbe77e3ee70d82496495205f21e0856d8f5
Unknown <buzgata@yahoo.ca>
2018-03-20 21:59:00 -0400

    Added Agent

---
+++ Assets/Scripts/Agent_CIA.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent_CIA : MonoBehaviour {
+
+	public Animation animation;
+
+	// Use this for initialization
+	void Start () {
+
+		animation = gameObject.GetComponent<Animation>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		animation.Play("walkbackwards");
+		
+	transform.position += new Vector3(0, 0, 0.4f);
+		}
+	}
