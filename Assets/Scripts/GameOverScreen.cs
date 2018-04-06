0dc95fb8da6ed6e23f1ec4b59bc084fe9f404fa7
Unknown <buzgata@yahoo.ca>
2018-04-05 20:45:30 -0400

    Worked a whole lot!

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Return)|| (Input.GetKeyDown (KeyCode.JoystickButton0))) {
+
+			SceneManager.LoadScene ("MainMenu");
+
+		}
+
+		
+	}
+}
