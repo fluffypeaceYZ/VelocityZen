972297466d314556c557340c34f1ae2d736af630
Yassine Zaabat <buzgata@yahoo.ca>
2020-11-22 04:41:05 -0500

    Fixed cannon and bullets for Car

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -21,7 +21,7 @@ public class Background : MonoBehaviour {
 		if (other.gameObject.tag == "Player") {
 			print ("New Background");
 			GameObject roadInstance;
-			roadInstance = Instantiate (Road, new Vector3 (0, 0, Player.transform.position.z + 1452.6f), Road.transform.rotation) as GameObject;
+			roadInstance = Instantiate (Road, new Vector3 (0, 0, Player.transform.position.z + 10), Road.transform.rotation) as GameObject;
 			GameObject fenceInstance;
 			fenceInstance = Instantiate (Fence, new Vector3 (-13, 3, Player.transform.position.z + 1461.6f), Fence.transform.rotation) as GameObject;
 			 
